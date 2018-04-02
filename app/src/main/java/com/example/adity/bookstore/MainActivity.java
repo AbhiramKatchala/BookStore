b813c72fe2317c3643d1035628fe4bf3ab538b85
GullipalliVijayaAditya <vijayaaaditya1996@gmail.com>
2018-04-02 15:19:20 +0530

    made small changes

--- app/src/main/java/com/example/adity/bookstore/MainActivity.java
+++ app/src/main/java/com/example/adity/bookstore/MainActivity.java
@@ -18,22 +18,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         lstBook = new ArrayList<>();
-        lstBook.add(new Book("The Vegitarian", "Categorie Book", "Description book", R.drawable.thevigitarian));
-        lstBook.add(new Book("The Wild Robot", "Categorie Book", "Description book", R.drawable.thewildrobot));
-        lstBook.add(new Book("Maria Semples", "Categorie Book", "Description book", R.drawable.mariasemples));
-        lstBook.add(new Book("The Martian", "Categorie Book", "Description book", R.drawable.themartian));
-        lstBook.add(new Book("He Died with...", "Categorie Book", "Description book", R.drawable.hediedwith));
-        lstBook.add(new Book("The Vegitarian", "Categorie Book", "Description book", R.drawable.thevigitarian));
-        lstBook.add(new Book("The Wild Robot", "Categorie Book", "Description book", R.drawable.thewildrobot));
-        lstBook.add(new Book("Maria Semples", "Categorie Book", "Description book", R.drawable.mariasemples));
-        lstBook.add(new Book("The Martian", "Categorie Book", "Description book", R.drawable.themartian));
-        lstBook.add(new Book("He Died with...", "Categorie Book", "Description book", R.drawable.hediedwith));
-        lstBook.add(new Book("The Vegitarian", "Categorie Book", "Description book", R.drawable.thevigitarian));
-        lstBook.add(new Book("The Wild Robot", "Categorie Book", "Description book", R.drawable.thewildrobot));
-        lstBook.add(new Book("Maria Semples", "Categorie Book", "Description book", R.drawable.mariasemples));
-        lstBook.add(new Book("The Martian", "Categorie Book", "Description book", R.drawable.themartian));
-        lstBook.add(new Book("He Died with...", "Categorie Book", "Description book", R.drawable.hediedwith));
-
+        lstBook.add(new Book("INDIAN POLITY","Categorie Book","Description book",R.drawable.aa));
+        lstBook.add(new Book("Just Friends","Categorie Book","Description book",R.drawable.bb));
+        lstBook.add(new Book("Top Publications","Categorie Book","Description book",R.drawable.cc));
+        lstBook.add(new Book("1100 Words","Categorie Book","Description book",R.drawable.dd));
+        lstBook.add(new Book("India","Categorie Book","Description book",R.drawable.ee));
+        lstBook.add(new Book("The Five Dollar Smile","Categorie Book","Description book",R.drawable.ff));
+        lstBook.add(new Book("The Great Indian Novel","Categorie Book","Description book",R.drawable.gg));
+        lstBook.add(new Book("History","Categorie Book","Description book",R.drawable.hh));
+        lstBook.add(new Book("Ashoka","Categorie Book","Description book",R.drawable.ii));
+        lstBook.add(new Book("Guide To India","Categorie Book","Description book",R.drawable.jj));
+        lstBook.add(new Book("The Times Of My Life","Categorie Book","Description book",R.drawable.kk));
+        lstBook.add(new Book("Brihat Parasara Hora Sastra","Categorie Book","Description book",R.drawable.ll));
+        lstBook.add(new Book("Raavan","Categorie Book","Description book",R.drawable.mm));
+        lstBook.add(new Book("Think India","Categorie Book","Description book",R.drawable.nn));
+        lstBook.add(new Book("Top Indian Children Picture Books","Categorie Book","Description book",R.drawable.oo));
         RecyclerView myrv = (RecyclerView) findViewById(R.id.recyclerview_id);
         RecyclerViewAdapter myAdapter = new RecyclerViewAdapter(this, lstBook);
         myrv.setLayoutManager(new GridLayoutManager(this, 3));
